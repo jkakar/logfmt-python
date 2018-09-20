27355f7de4d34795eb59dccec3dd6999013c30a2
Kit La Touche <kit@transneptune.net>
2018-09-20 09:55:35 -0600

    Add LogfmtFormatter (#8)
    And do some PEP8-ish formatting.

--- logfmt/formatter.py
+++ logfmt/formatter.py
@@ -1,27 +1,31 @@
 import numbers
-
-def format_line(hash):
-  outarr = []
-  for k,v in hash.items():
-    if v is None:
-      outarr.append("%s=" % k)
-      continue
-
-    if isinstance(v, bool):
-        v = "true" if v else "false"
-
-    elif isinstance(v, numbers.Number ):
-        pass
-
-    else:
-      if isinstance(v, (dict, object)):
-        v = str(v)
-
-      v = '"%s"' % v.replace('"', '\\' + '"')
-    
-    outarr.append("%s=%s" % (k, v))
-
-  return " ".join(outarr)
+import logging
 
 
+def format_line(extra):
+    outarr = []
+    for k, v in extra.items():
+        if v is None:
+            outarr.append("%s=" % k)
+            continue
 
+        if isinstance(v, bool):
+            v = "true" if v else "false"
+        elif isinstance(v, numbers.Number):
+            pass
+        else:
+            if isinstance(v, (dict, object)):
+                v = str(v)
+            v = '"%s"' % v.replace('"', '\\' + '"')
+        outarr.append("%s=%s" % (k, v))
+    return " ".join(outarr)
+
+
+class LogfmtFormatter(logging.Formatter):
+    def format(self, record):
+        return ' '.join([
+            f'at={record.levelname}',
+            f'msg="{record.msg}"',
+            f'process={record.processName}',
+            format_line(getattr(record, 'context', {})),
+        ])
